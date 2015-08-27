031d0577cce2297e3bcbe6cf8896311c81ccfdc2
Rik Meijer <rikmeijer@gmail.com>
2015-08-27 22:30:41 +0200

    add class dependencies

--- src/PHPClass.php
+++ src/PHPClass.php
@@ -3,7 +3,22 @@ namespace ActiveRecord;
 
 class PHPClass
 {
+	/**
+	 * 
+	 * @var string
+	 */
 	private $name;
+	
+	/**
+	 * 
+	 * @var array
+	 */
+	private $constructorArguments = array();
+
+	/**
+	 * 
+	 * @var array
+	 */
 	private $memberVariables = array();
 	
 	public function __construct($name)
@@ -22,12 +37,28 @@ class PHPClass
 		);
 	}
 	
+	public function dependsOn($name)
+	{
+		$this->addPrivateInstanceVariable($name);
+		$this->constructorArguments[$name] = array();
+	}
+	
 	/**
 	 * @return string
 	 */
 	public function generate()
 	{
 		$lines = array("class " . $this->name, "{");
+		if (count($this->constructorArguments) > 0) {
+			$constructorArguments = array();
+			foreach ($this->constructorArguments as $constructorArgumentIdentifier => $constructorArgument) {
+				$constructorArguments[] = '$' . $constructorArgumentIdentifier;
+			}
+			$lines[] = "\tpublic function __construct(" . join(", ", $constructorArguments) . ")";
+			$lines[] = "\t{";
+			$lines[] = "\t\$this->" . $constructorArgumentIdentifier . ' = $' . $constructorArgumentIdentifier . ';';
+			$lines[] = "\t}";
+		}
 		foreach ($this->memberVariables as $memberVariableIdentifier => $memberVariable) {
 			$lines[] = "\t" . $memberVariable['access'] . ' $' . $memberVariableIdentifier . ';';
 		}
--- tests/src/PHPClassTest.php
+++ tests/src/PHPClassTest.php
@@ -6,7 +6,9 @@ class PHPClassTest extends \PHPUnit_Framework_TestCase
 	public function testGeneratePHPClassGivesEmptyPHPClass()
 	{
 		$class = new PHPClass("ActiveRecord");
-		$this->assertEquals("class ActiveRecord" . PHP_EOL . "{" . PHP_EOL . "}" . PHP_EOL . "", $class->generate());
+		$this->assertEquals("class ActiveRecord" . PHP_EOL . 
+				"{" . PHP_EOL . 
+				"}" . PHP_EOL . "", $class->generate());
 	}
 	
 
@@ -14,6 +16,24 @@ class PHPClassTest extends \PHPUnit_Framework_TestCase
 	{
 		$class = new PHPClass("PersonRecord");
 		$class->addPrivateInstanceVariable("name");
-		$this->assertEquals("class PersonRecord" . PHP_EOL . "{" . PHP_EOL . "\tprivate \$name;" . PHP_EOL . "}" . PHP_EOL . "", $class->generate());
+		$this->assertEquals("class PersonRecord" . PHP_EOL . 
+				"{" . PHP_EOL . 
+				"\tprivate \$name;" . PHP_EOL . 
+				"}" . PHP_EOL . "", $class->generate());
+	}
+	
+
+	public function testPHPClassWithDependency()
+	{
+		$class = new PHPClass("PersonRecord");
+		$class->dependsOn("repository");
+		$this->assertEquals("class PersonRecord" . PHP_EOL . 
+				"{" . PHP_EOL . 
+				"\tpublic function __construct(\$repository)" . PHP_EOL . 
+				"\t{" . PHP_EOL .
+				"\t\$this->repository = \$repository;" . PHP_EOL .
+				"\t}" . PHP_EOL .
+				"\tprivate \$repository;" . PHP_EOL . 
+				"}" . PHP_EOL . "", $class->generate());
 	}
 }
