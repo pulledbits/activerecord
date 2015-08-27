07b9cf189138989ce682eeeac9625b6fa6b2ba72
Rik Meijer <rikmeijer@gmail.com>
2015-08-27 22:03:17 +0200

    rename test to properly reflect what is expected

--- tests/src/PHPClassTest.php
+++ tests/src/PHPClassTest.php
@@ -3,7 +3,7 @@ namespace ActiveRecord;
 
 class PHPClassTest extends \PHPUnit_Framework_TestCase
 {
-	public function testCreateNewPHPClass()
+	public function testGeneratePHPClassGivesEmptyPHPClass()
 	{
 		$class = new PHPClass("ActiveRecord");
 		$this->assertEquals("class ActiveRecord\n{\n}\n", $class->generate());
