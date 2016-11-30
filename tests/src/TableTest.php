af714376905096cb91501fbe5f753e02e1e0f64e
Rik Meijer <rikmeijer@gmail.com>
2016-11-30 12:42:11 +0100

    fix naming

--- tests/src/TableTest.php
+++ tests/src/TableTest.php
@@ -4,7 +4,7 @@ namespace ActiveRecord;
 class TableTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testMakeClass_When_DefaultState_Expect_ArrayWithClassIdentifier()
+    public function testDescribe_When_DefaultState_Expect_ArrayWithClassIdentifier()
     {
         $table = new Table(new class() extends \Doctrine\DBAL\Schema\Table {
 
@@ -20,7 +20,7 @@ class TableTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($classDescription['identifier'], '\\Database\\Table\\MyTable');
     }
 
-    public function testMakeClass_When_DifferingTableName_Expect_ArrayWithClassIdentifierAndDifferentClassName()
+    public function testDescribe_When_DifferingTableName_Expect_ArrayWithClassIdentifierAndDifferentClassName()
     {
         $table = new Table(new class() extends \Doctrine\DBAL\Schema\Table {
 
