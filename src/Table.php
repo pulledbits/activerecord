8d96e77406ba0dd231ca3c4412162c4fdcf90760
Rik Meijer <rikmeijer@gmail.com>
2016-12-01 12:01:17 +0100

    refactor table code

--- src/Table.php
+++ src/Table.php
@@ -23,29 +23,35 @@ final class Table
     private function describeQuery(string $queryType, array $query) : array {
         return [$queryType, $query];
     }
+    private function describeQuerySelect(string $fields, string $from, array $where) : array {
+        return $this->describeQuery('SELECT', [
+            'fields' => $fields,
+            'from' => $from,
+            'where' => join(' AND ', $where)
+        ]);
+    }
     
     public function describe($namespace) : array {
         if (substr($namespace, -1) != "\\") {
             $namespace .= "\\";
         }
         
+        $tableIdentifier = $this->dbalSchemaTable->getName();
+        
         $methods = [
-            'fetchAll' => $this->describeQueryMethod([], $this->describeQuery('SELECT', [
-                'fields' => '*',
-                'from' => $this->dbalSchemaTable->getName()
-            ]))
+            'fetchAll' => $this->describeQueryMethod([], $this->describeQuerySelect('*', $tableIdentifier, []))
         ];
         foreach ($this->dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
             $words = explode('_', $foreignKeyIdentifier);
             $camelCased = array_map('ucfirst', $words);
             $foreignKeyMethodIdentifier = join('', $camelCased);
-            $methods["fetchBy" . $foreignKeyMethodIdentifier] = $this->describeQueryMethod($foreignKey->getLocalColumns(), $this->describeQuery('SELECT', [
-                'fields' => '*',
-                'from' => $this->dbalSchemaTable->getName(),
-                'where' => join(' AND ', array_map(function($methodParameter) {
-                    return $methodParameter . ' = :' . $methodParameter;
-                }, $foreignKey->getLocalColumns()))
-            ]));
+            
+            $where = array_map(function($methodParameter) {
+                return $methodParameter . ' = :' . $methodParameter;
+            }, $foreignKey->getLocalColumns());
+            $query = $this->describeQuerySelect('*', $tableIdentifier, $where);
+            
+            $methods["fetchBy" . $foreignKeyMethodIdentifier] = $this->describeQueryMethod($foreignKey->getLocalColumns(), $query);
         }
         
         return [
