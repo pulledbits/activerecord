ecdad4d0dd7628591c24f5c781ed33ba1b99200d
Rik Meijer <rikmeijer@gmail.com>
2016-12-05 13:12:34 +0100

    supply classes in description

--- src/SourceSchema.php
+++ src/SourceSchema.php
@@ -9,12 +9,31 @@
 namespace ActiveRecord;
 
 
+use Doctrine\DBAL\Schema\AbstractSchemaManager;
+
 class SourceSchema
 {
+    /**
+     * @var Doctrine\DBAL\Schema\AbstractSchemaManager
+     */
+    private $schemaManager;
+
+    public function __construct(AbstractSchemaManager $schemaManager)
+    {
+        $this->schemaManager = $schemaManager;
+    }
+
     public function describe()
     {
+        $classes = [];
+
+        foreach ($this->schemaManager->listTables() as $table) {
+            $tableName = $table->getName();
+            $classes[$tableName] = ['identifier' => $tableName];
+        }
+
         return [
-            'classes' => [1]
+            'classes' => $classes
         ];
     }
 }
--- tests/src/SourceSchemaTest.php
+++ tests/src/SourceSchemaTest.php
@@ -14,14 +14,26 @@ class SchemaTest extends \PHPUnit_Framework_TestCase
 
     public function testDescribe_When_Default_Expect_ArrayWithClasses()
     {
-        $schema = new SourceSchema(new class() extends \Doctrine\DBAL\Schema {
-            public function __construct()
-            {}
+        $schema = new SourceSchema(new class() extends \Doctrine\DBAL\Schema\MySqlSchemaManager {
+            public function __construct() {}
+
+            public function listTables() {
+                return [
+                    new class extends \Doctrine\DBAL\Schema\Table
+                    {
+                        public function __construct() {}
+                        public function getName() {
+                            return 'MyTable';
+                        }
+                    }
+                ];
+            }
         });
 
         $schemaDescription = $schema->describe();
 
         $this->assertCount(1, $schemaDescription['classes']);
+        $this->assertEquals('MyTable', $schemaDescription['classes']['MyTable']['identifier']);
     }
 
 }
