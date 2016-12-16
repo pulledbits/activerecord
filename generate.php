ca68361314e28628aa0e0af2d1538f8b38a88198
Rik Meijer <rikmeijer@gmail.com>
2016-12-16 10:37:24 +0100

    cleanup generate.php

--- generate.php
+++ generate.php
@@ -20,7 +20,6 @@ if (file_exists($recordsDirectory) == false) {
 }
 
 $config = new \Doctrine\DBAL\Configuration();
-//..
 $connectionParams = array(
     'url' => 'mysql://teach:teach@localhost/teach'
 );
@@ -28,44 +27,30 @@ $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
 
 $generator = new CodeGenerator();
 
-function createMethod(string $identifier, array $parameters, array $body) {
-    $method = PhpMethod::create($identifier);
-    foreach ($parameters as $methodParameterIdentifier => $methodParameterType) {
-        $byReference = false;
-        if (substr($methodParameterIdentifier,0,1) === '&') {
-            $byReference = true;
-            $methodParameterIdentifier = substr($methodParameterIdentifier,1);
-        }
-        $parameter = PhpParameter::create($methodParameterIdentifier);
-        $parameter->setType($methodParameterType);
-        $parameter->setPassedByReference($byReference);
-        $method->addParameter($parameter);
-    }
-    $method->setBody(join(PHP_EOL, $body));
-    return $method;
-}
-
-function createPHPFile(string $filename, string $code) {
-    file_put_contents($filename, '<?php' . PHP_EOL . $code);
-}
-
-
 $sourceSchema = new \ActiveRecord\Source\Schema($conn->getSchemaManager());
-
 $schemaDescription = $sourceSchema->describe($targetNamespace);
-
 foreach ($schemaDescription['recordClasses'] as $tableName => $recordClassDescription) {
     $recordClass = new gossi\codegen\model\PhpClass($recordClassDescription['identifier']);
     $recordClass->setFinal(true);
 
     foreach ($recordClassDescription['properties'] as $propertyIdentifier => $propertyType) {
-        $recordClass->setProperty(PhpProperty::create($propertyIdentifier)->setType($propertyType)->setVisibility('private'));
+        $property = PhpProperty::create($propertyIdentifier);
+        $property->setType($propertyType);
+        $property->setVisibility('private');
+        $recordClass->setProperty($property);
     }
 
     foreach ($recordClassDescription['methods'] as $methodIdentifier => $methodDescription) {
-        $recordClass->setMethod(createMethod($methodIdentifier, $methodDescription['parameters'], $methodDescription['body']));
+        $method = PhpMethod::create($methodIdentifier);
+        foreach ($methodDescription['parameters'] as $methodParameterIdentifier => $methodParameterType) {
+            $parameter = PhpParameter::create($methodParameterIdentifier);
+            $parameter->setType($methodParameterType);
+            $method->addParameter($parameter);
+        }
+        $method->setBody(join(PHP_EOL, $methodDescription['body']));
+        $recordClass->setMethod($method);
     }
-    createPHPFile($recordsDirectory . DIRECTORY_SEPARATOR . $tableName . '.php', $generator->generate($recordClass));
+    file_put_contents($recordsDirectory . DIRECTORY_SEPARATOR . $tableName . '.php', '<?php' . PHP_EOL . $generator->generate($recordClass));
 }
 
 // test activiteit
