b9bd62d881cf498b65204f5be209070c5e13318f
Rik Meijer <rikmeijer@gmail.com>
2017-01-06 20:08:16 +0100

    move test records to  bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,156 +1,169 @@
 <?php
-namespace ActiveRecord\Test;
-/* 
- * test specific bootstrapper
- */
-$applicationBootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$applicationBootstrap();
-
-
-function createMockSchema(array $tables) {
-    return new \ActiveRecord\Source\Schema(new class($tables) extends \Doctrine\DBAL\Schema\MySqlSchemaManager {
-
-        private $tables;
-
-        public function __construct(array $tables) {
-            foreach ($tables as $tableIdentifier => $columns) {
-                $this->tables[] = \ActiveRecord\Test\createMockTable($tableIdentifier, $columns);
-            }
-        }
-
-        public function listTables() {
-            return [
-                new class extends \Doctrine\DBAL\Schema\Table
-                {
-                    public function __construct() {}
-                    public function getName() {
-                        return 'MyTable';
-                    }
-                }
-            ];
-        }
-    });
-}
-
-function createMockTable(string $tableIdentifier, array $columns) {
-    return new class($tableIdentifier, $columns) extends \Doctrine\DBAL\Schema\Table {
-
-        private $tableIdentifier;
-        private $primaryKey;
-        private $foreignKeys;
-        private $columns;
-
-        public function __construct(string $tableIdentifier, array $columns)
-        {
-            $this->tableIdentifier = $tableIdentifier;
-            $this->columns = [];
-            $this->primaryKey = [];
-            $foreignKeys = [];
-            foreach ($columns as $columnIdentifier => $column) {
-                $this->columns[$columnIdentifier] = new class extends \Doctrine\DBAL\Schema\Column {public function __construct(){}};
-                if ($column['primaryKey']) {
-                    $this->primaryKey[] = $columnIdentifier;
-                }
-                if (array_key_exists('references', $column)) {
-                    foreach ($column['references'] as $foreignKeyIdentifier => $foreignKey) {
-                        if (array_key_exists($foreignKeyIdentifier, $foreignKeys) === false) {
-                            $foreignKeys[$foreignKeyIdentifier] = [
-                                'table' => $foreignKey[0],
-                                'columns' => [],
-                                'foreignColumns' => []
-                            ];
-                        }
-
-                        $foreignKeys[$foreignKeyIdentifier]['columns'][] = $columnIdentifier;
-                        $foreignKeys[$foreignKeyIdentifier]['foreignColumns'][] = $foreignKey[1];
-                    }
-                }
-            }
-
-            $this->foreignKeys = [];
-            foreach ($foreignKeys as $foreignKeyIdentifier => $foreignKey) {
-                $this->foreignKeys[$foreignKeyIdentifier] = new \Doctrine\DBAL\Schema\ForeignKeyConstraint($foreignKey['columns'], $foreignKey['table'], $foreignKey['foreignColumns'], $foreignKeyIdentifier);
-            }
-
-        }
-
-        public function getName()
-        {
-            return $this->tableIdentifier;
-        }
-        public function getColumns()
-        {
-            return $this->columns;
-        }
-        public function hasPrimaryKey() {
-            return count($this->primaryKey) > 0;
-        }
-        public function getPrimaryKeyColumns() {
-            return $this->primaryKey;
-        }
-        public function getForeignKeys()
-        {
-            return $this->foreignKeys;
-        }
-    };
-}
-
-
-
-function createMockPDO(string $query, array $results) {
-    return new class($query, $results) extends \PDO
+namespace Test\Record {
+    class activiteit implements \ActiveRecord\WritableRecord
     {
 
-        private $query;
-        private $results;
-
-        public function __construct(string $query, array $results)
+        /**
+         */
+        public function delete()
         {
-            $this->query = $query;
-            $this->results = $results;
         }
 
-        public function prepare($query, $options = null)
+        public function __set($property, $value)
         {
-            if (preg_match($this->query, $query, $match) === 1) {
-                return new class($this->results) extends \PDOStatement
-                {
-                    private $results;
-
-                    public function __construct(array $results)
-                    {
-                        $this->results = $results;
-                    }
-
-                    public function fetchAll($how = NULL, $class_name = NULL, $ctor_args = NULL)
-                    {
-                        if ($how === \PDO::FETCH_ASSOC) {
-                            return $this->results;
-                        }
-                    }
-                };
-            }
         }
-    };
-}
 
-function createMockPDOMultiple(array $queries) : \PDO {
-    return new class($queries) extends \PDO
+        public function __get($property)
+        {
+            return 'newName';
+        }
+    }
+
+    class thema implements \ActiveRecord\ReadableRecord
     {
 
-        private $queries;
-        private $results;
-
-        public function __construct(array $queries)
+        public function __get($property)
         {
-            $this->queries = $queries;
+            return 'newName';
         }
+    }
+}
 
-        public function prepare($query, $options = null)
+namespace ActiveRecord\Test {
+    /*
+     * test specific bootstrapper
+     */
+    $applicationBootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+    $applicationBootstrap();
+
+    function createMockSchema(array $tables)
+    {
+        return new \ActiveRecord\Source\Schema(new class($tables) extends \Doctrine\DBAL\Schema\MySqlSchemaManager
         {
-            foreach ($this->queries as $expectedQuery => $results) {
-                if (preg_match($expectedQuery, $query, $match) === 1) {
-                    return new class($results) extends \PDOStatement
+
+            private $tables;
+
+            public function __construct(array $tables)
+            {
+                foreach ($tables as $tableIdentifier => $columns) {
+                    $this->tables[] = \ActiveRecord\Test\createMockTable($tableIdentifier, $columns);
+                }
+            }
+
+            public function listTables()
+            {
+                return [
+                    new class extends \Doctrine\DBAL\Schema\Table
+                    {
+                        public function __construct()
+                        {
+                        }
+
+                        public function getName()
+                        {
+                            return 'MyTable';
+                        }
+                    }
+                ];
+            }
+        });
+    }
+
+    function createMockTable(string $tableIdentifier, array $columns)
+    {
+        return new class($tableIdentifier, $columns) extends \Doctrine\DBAL\Schema\Table
+        {
+
+            private $tableIdentifier;
+            private $primaryKey;
+            private $foreignKeys;
+            private $columns;
+
+            public function __construct(string $tableIdentifier, array $columns)
+            {
+                $this->tableIdentifier = $tableIdentifier;
+                $this->columns = [];
+                $this->primaryKey = [];
+                $foreignKeys = [];
+                foreach ($columns as $columnIdentifier => $column) {
+                    $this->columns[$columnIdentifier] = new class extends \Doctrine\DBAL\Schema\Column
+                    {
+                        public function __construct()
+                        {
+                        }
+                    };
+                    if ($column['primaryKey']) {
+                        $this->primaryKey[] = $columnIdentifier;
+                    }
+                    if (array_key_exists('references', $column)) {
+                        foreach ($column['references'] as $foreignKeyIdentifier => $foreignKey) {
+                            if (array_key_exists($foreignKeyIdentifier, $foreignKeys) === false) {
+                                $foreignKeys[$foreignKeyIdentifier] = [
+                                    'table' => $foreignKey[0],
+                                    'columns' => [],
+                                    'foreignColumns' => []
+                                ];
+                            }
+
+                            $foreignKeys[$foreignKeyIdentifier]['columns'][] = $columnIdentifier;
+                            $foreignKeys[$foreignKeyIdentifier]['foreignColumns'][] = $foreignKey[1];
+                        }
+                    }
+                }
+
+                $this->foreignKeys = [];
+                foreach ($foreignKeys as $foreignKeyIdentifier => $foreignKey) {
+                    $this->foreignKeys[$foreignKeyIdentifier] = new \Doctrine\DBAL\Schema\ForeignKeyConstraint($foreignKey['columns'], $foreignKey['table'], $foreignKey['foreignColumns'], $foreignKeyIdentifier);
+                }
+
+            }
+
+            public function getName()
+            {
+                return $this->tableIdentifier;
+            }
+
+            public function getColumns()
+            {
+                return $this->columns;
+            }
+
+            public function hasPrimaryKey()
+            {
+                return count($this->primaryKey) > 0;
+            }
+
+            public function getPrimaryKeyColumns()
+            {
+                return $this->primaryKey;
+            }
+
+            public function getForeignKeys()
+            {
+                return $this->foreignKeys;
+            }
+        };
+    }
+
+
+    function createMockPDO(string $query, array $results)
+    {
+        return new class($query, $results) extends \PDO
+        {
+
+            private $query;
+            private $results;
+
+            public function __construct(string $query, array $results)
+            {
+                $this->query = $query;
+                $this->results = $results;
+            }
+
+            public function prepare($query, $options = null)
+            {
+                if (preg_match($this->query, $query, $match) === 1) {
+                    return new class($this->results) extends \PDOStatement
                     {
                         private $results;
 
@@ -168,6 +181,45 @@ function createMockPDOMultiple(array $queries) : \PDO {
                     };
                 }
             }
-        }
-    };
+        };
+    }
+
+    function createMockPDOMultiple(array $queries): \PDO
+    {
+        return new class($queries) extends \PDO
+        {
+
+            private $queries;
+            private $results;
+
+            public function __construct(array $queries)
+            {
+                $this->queries = $queries;
+            }
+
+            public function prepare($query, $options = null)
+            {
+                foreach ($this->queries as $expectedQuery => $results) {
+                    if (preg_match($expectedQuery, $query, $match) === 1) {
+                        return new class($results) extends \PDOStatement
+                        {
+                            private $results;
+
+                            public function __construct(array $results)
+                            {
+                                $this->results = $results;
+                            }
+
+                            public function fetchAll($how = NULL, $class_name = NULL, $ctor_args = NULL)
+                            {
+                                if ($how === \PDO::FETCH_ASSOC) {
+                                    return $this->results;
+                                }
+                            }
+                        };
+                    }
+                }
+            }
+        };
+    }
 }
--- tests/src/TableTest.php
+++ tests/src/TableTest.php
@@ -1,199 +1,162 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hameijer
- * Date: 15-12-16
- * Time: 15:16
- */
-namespace Test\Record {
-    class activiteit implements \ActiveRecord\WritableRecord
+namespace ActiveRecord;
+
+class TableTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTransformColumnToProperty_When_ColumnIdentifierSupplied_Expect_ColumnIdPrefixedWithUnderscore()
     {
 
-        /**
-         */
-        public function delete()
-        {
-        }
+        $table = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', [])));
 
-        public function __set($property, $value)
-        {
-        }
-
-        public function __get($property)
-        {
-            return 'newName';
-        }
+        $this->assertEquals("_id", $table->transformColumnToProperty('id'));
     }
 
-    class thema implements \ActiveRecord\ReadableRecord
+    public function testSelect_When_NoWhereParametersSupplied_Expect_FiveRecords()
     {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id, name FROM activiteit/', [
+            [],
+            [],
+            [],
+            [],
+            [],
+        ])));
 
-        public function __get($property)
-        {
-            return 'newName';
-        }
+        $this->assertCount(5, $schema->select(['id', 'name'], []));
     }
-}
 
-namespace ActiveRecord {
-
-    class TableTest extends \PHPUnit_Framework_TestCase
+    public function testSelect_When_SpecificWhereParameterSupplied_Expect_ThreeRecords()
     {
-        public function testTransformColumnToProperty_When_ColumnIdentifierSupplied_Expect_ColumnIdPrefixedWithUnderscore()
-        {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT name FROM activiteit WHERE name = (?<namedParameter>:(\w+))/', [
+                [],
+                [],
+                []
+            ]
+        )));
 
-            $table = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', [])));
+        $records = $schema->select(['name'], ['name' => 'foo']);
 
-            $this->assertEquals("_id", $table->transformColumnToProperty('id'));
-        }
+        $this->assertCount(3, $records);
+    }
 
-        public function testSelect_When_NoWhereParametersSupplied_Expect_FiveRecords()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id, name FROM activiteit/', [
+    public function testSelect_When_MultipleWhereParametersSupplied_Expect_OneRecord()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/', [
+                [],
+            ]
+        )));
+
+        $records = $schema->select(['name'], ['name' => 'foo', 'id' => '1']);
+
+        $this->assertCount(1, $records);
+    }
+
+    public function testSelectFrom_When_NoWhereParametersSupplied_Expect_FiveRecords()
+    {
+        $schema = new Table('thema', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id, name FROM activiteit/', [
                 [],
                 [],
                 [],
                 [],
                 [],
-            ])));
+            ]
+        )));
 
-            $this->assertCount(5, $schema->select(['id', 'name'], []));
-        }
+        $this->assertCount(5, $schema->selectFrom('activiteit', ['id', 'name'], []));
+    }
 
-        public function testSelect_When_SpecificWhereParameterSupplied_Expect_ThreeRecords()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT name FROM activiteit WHERE name = (?<namedParameter>:(\w+))/', [
-                    [],
-                    [],
-                    []
-                ]
-            )));
-
-            $records = $schema->select(['name'], ['name' => 'foo']);
-
-            $this->assertCount(3, $records);
-        }
-
-        public function testSelect_When_MultipleWhereParametersSupplied_Expect_OneRecord()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/', [
-                    [],
-                ]
-            )));
-
-            $records = $schema->select(['name'], ['name' => 'foo', 'id' => '1']);
-
-            $this->assertCount(1, $records);
-        }
-
-        public function testSelectFrom_When_NoWhereParametersSupplied_Expect_FiveRecords()
-        {
-            $schema = new Table('thema', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id, name FROM activiteit/', [
+    public function testUpdate_When_NoWhereParametersSupplied_Expect_FiveUpdates()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/UPDATE activiteit SET name = (?<namedSet1>:(\w+))/' => [],
+                '/SELECT name FROM activiteit/' => [
                     [],
                     [],
                     [],
                     [],
                     [],
                 ]
-            )));
+            ])
+        ));
 
-            $this->assertCount(5, $schema->selectFrom('activiteit', ['id', 'name'], []));
-        }
+        $this->assertCount(5, $schema->update(['name' => 'newName'], []));
+    }
 
-        public function testUpdate_When_NoWhereParametersSupplied_Expect_FiveUpdates()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/UPDATE activiteit SET name = (?<namedSet1>:(\w+))/' => [],
-                    '/SELECT name FROM activiteit/' => [
-                        [],
-                        [],
-                        [],
-                        [],
-                        [],
+    public function testUpdate_When_SpecificWhereParameterSupplied_Expect_ThreeUpdates()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/UPDATE activiteit SET name = (?<namedSet1>:(\w+)) WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/' => [],
+                '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/' => [
+                    [],
+                    [],
+                    [],
+                ]
+            ])
+        ));
+
+        $this->assertCount(3, $schema->update(['name' => 'newName'], ['name' => 'oldName', 'id' => '1']));
+    }
+
+    public function testUpdate_When_MultipleWhereParameterSupplied_Expect_ThreeUpdates()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/UPDATE activiteit SET name = (?<namedSet1>:(\w+)) WHERE name = (?<namedParameter1>:(\w+))/' => [],
+                '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+))/' => [
+                    [],
+                    [],
+                    [],
+                ]
+            ])
+        ));
+
+        $this->assertCount(3, $schema->update(['name' => 'newName'], ['name' => 'oldName']));
+    }
+
+
+    public function testDelete_When_SingleParameter_Expect_One()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/DELETE FROM activiteit WHERE name = (?<namedSet1>:(\w+))/' => [],
+                '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+))/' => [
+                    [],
+                ]
+            ])
+        ));
+
+        $this->assertCount(1, $schema->delete(['name' => 'newName']));
+    }
+
+
+    public function testDelete_When_MultipleParameters_Expect_Five()
+    {
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/DELETE FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [],
+                '/SELECT id, name FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                ]
+            ])
+        ));
+
+        $this->assertCount(5, $schema->delete(['id' => '1', 'name' => 'newName']));
+    }
+
+    public function testInsert_When_NoWhereParametersSupplied_Expect_InsertedRecord()
+    {
+
+        $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
+                '/INSERT INTO activiteit \(id, name\) VALUES \((?<namedSet1>:(\w+)), (?<namedSet2>:(\w+))\)/' => [],
+                '/SELECT id, name FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [
+                    [
+                        'id' => '1',
+                        'name' => 'newName'
                     ]
-                ])
-            ));
+                ]
+            ])
+        ));
 
-            $this->assertCount(5, $schema->update(['name' => 'newName'], []));
-        }
-
-        public function testUpdate_When_SpecificWhereParameterSupplied_Expect_ThreeUpdates()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/UPDATE activiteit SET name = (?<namedSet1>:(\w+)) WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/' => [],
-                    '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+)) AND id = (?<namedParameter2>:(\w+))/' => [
-                        [],
-                        [],
-                        [],
-                    ]
-                ])
-            ));
-
-            $this->assertCount(3, $schema->update(['name' => 'newName'], ['name' => 'oldName', 'id' => '1']));
-        }
-
-        public function testUpdate_When_MultipleWhereParameterSupplied_Expect_ThreeUpdates()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/UPDATE activiteit SET name = (?<namedSet1>:(\w+)) WHERE name = (?<namedParameter1>:(\w+))/' => [],
-                    '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+))/' => [
-                        [],
-                        [],
-                        [],
-                    ]
-                ])
-            ));
-
-            $this->assertCount(3, $schema->update(['name' => 'newName'], ['name' => 'oldName']));
-        }
-
-
-        public function testDelete_When_SingleParameter_Expect_One()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/DELETE FROM activiteit WHERE name = (?<namedSet1>:(\w+))/' => [],
-                    '/SELECT name FROM activiteit WHERE name = (?<namedParameter1>:(\w+))/' => [
-                        [],
-                    ]
-                ])
-            ));
-
-            $this->assertCount(1, $schema->delete(['name' => 'newName']));
-        }
-
-
-        public function testDelete_When_MultipleParameters_Expect_Five()
-        {
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/DELETE FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [],
-                    '/SELECT id, name FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [
-                        [],
-                        [],
-                        [],
-                        [],
-                        [],
-                    ]
-                ])
-            ));
-
-            $this->assertCount(5, $schema->delete(['id' => '1', 'name' => 'newName']));
-        }
-
-        public function testInsert_When_NoWhereParametersSupplied_Expect_InsertedRecord()
-        {
-
-            $schema = new Table('activiteit', new Schema('\Test\Record', \ActiveRecord\Test\createMockPDOMultiple([
-                    '/INSERT INTO activiteit \(id, name\) VALUES \((?<namedSet1>:(\w+)), (?<namedSet2>:(\w+))\)/' => [],
-                    '/SELECT id, name FROM activiteit WHERE id = (?<namedSet1>:(\w+)) AND name = (?<namedSet2>:(\w+))/' => [
-                        [
-                            'id' => '1',
-                            'name' => 'newName'
-                        ]
-                    ]
-                ])
-            ));
-
-            $this->assertEquals('newName', $schema->insert(['id' => '1', 'name' => 'newName'])[0]->name);
-        }
+        $this->assertEquals('newName', $schema->insert(['id' => '1', 'name' => 'newName'])[0]->name);
     }
 }
