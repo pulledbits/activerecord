88357d987d56a2181a139983920bd1f0451a49a9
Rik Meijer <rikmeijer@gmail.com>
2017-01-11 14:16:29 +0100

    cleanup describe method

--- src/Source/Table.php
+++ src/Source/Table.php
@@ -15,6 +15,9 @@ final class Table
     public function __construct(string $namespace)
     {
         $this->namespace = $namespace;
+        if (substr($this->namespace, -1) != "\\") {
+            $this->namespace .= "\\";
+        }
     }
 
     private function describeMethod(bool $static, array $parameters, array $body) : array {
@@ -25,81 +28,88 @@ final class Table
         ];
     }
 
+    private function describeConstructorMethod() : array {
+        return $this->describeMethod(false, ["table" => '\ActiveRecord\Table', "values" => 'array'], [
+            '$this->table = $table;',
+            '$this->values = $values;'
+        ]);
+    }
+
+    private function describeSetterMethod() {
+        return $this->describeMethod(false, ["property" => 'string', "value" => 'string'], [
+            'if (count($this->table->update([$property => $this->__get($property)], $this->primaryKey())) > 0) {',
+            '$this->values[$property] = $value;',
+            '}'
+        ]);
+    }
+
+    private function describeGetterMethod() {
+        return $this->describeMethod(false, ["property" => 'string'], [
+            'return $this->values[$property];'
+        ]);
+    }
+
+    private function describePrimaryKeyMethod(\Doctrine\DBAL\Schema\Table $dbalSchemaTable) {
+        $primaryKeyWhere = [];
+        if ($dbalSchemaTable->hasPrimaryKey()) {
+            foreach ($dbalSchemaTable->getPrimaryKeyColumns() as $columnIdentifier) {
+                $primaryKeyWhere[] = $this->makeArrayMappingToProperty($columnIdentifier, $columnIdentifier);
+            }
+        }
+        return $this->describeMethod(false, [], [
+            'return [' . join(', ', $primaryKeyWhere) . '];'
+        ]);
+    }
+
+    private function describeDeleteMethod() {
+        return $this->describeMethod(false, [], [
+            'return $this->table->delete($this->primaryKey());'
+        ]);
+    }
+
+    private function describeFetchByFKMethod(\Doctrine\DBAL\Schema\ForeignKeyConstraint $foreignKey) {
+        $fkLocalColumns = $foreignKey->getLocalColumns();
+        $where = array_combine($foreignKey->getForeignColumns(), $fkLocalColumns);
+
+        $whereParameters = [];
+        foreach ($where as $referencedColumnName => $parameterIdentifier) {
+            $whereParameters[] = $this->makeArrayMappingToProperty($referencedColumnName, $parameterIdentifier);
+        }
+
+        $query = ['return $this->table->selectFrom("' . $foreignKey->getForeignTableName() . '", [\'' . join('\', \'', $foreignKey->getForeignColumns()) . '\'], [', join(',' . PHP_EOL, $whereParameters), ']);'];
+
+        return $this->describeMethod(false, [], $query);
+    }
+
     private function makeArrayMapping(string $keyIdentifier, string $variableIdentifier) : string {
         return '\'' . $keyIdentifier . '\' => ' . $variableIdentifier;
     }
     private function makeArrayMappingToProperty(string $keyIdentifier, string $propertyIdentifier) {
         return $this->makeArrayMapping($keyIdentifier, '$this->__get(\'' . $propertyIdentifier . '\')');
     }
+
     
     public function describe(\Doctrine\DBAL\Schema\Table $dbalSchemaTable) : array {
-        if (substr($this->namespace, -1) != "\\") {
-            $this->namespace .= "\\";
-        }
-
-        $columnIdentifiers = array_keys($dbalSchemaTable->getColumns());
-
-        $tableIdentifier = $dbalSchemaTable->getName();
-
         $methods = [
-            '__construct' => $this->describeMethod(false, ["table" => '\ActiveRecord\Table', "values" => 'array'], [
-                '$this->table = $table;',
-                '$this->values = $values;'
-            ])
+            '__construct' => $this->describeConstructorMethod(),
+            '__set' => $this->describeSetterMethod(),
+            '__get' => $this->describeGetterMethod(),
+            'primaryKey' => $this->describePrimaryKeyMethod($dbalSchemaTable),
+            'delete' => $this->describeDeleteMethod()
         ];
 
-        $primaryKeyWhere = [];
-        $properties = [
-            'table' => ['\ActiveRecord\Table', ['static' => false, 'value' => null]],
-            'values' => ['array', ['static' => false, 'value' => null]]
-        ];
-        foreach ($columnIdentifiers as $columnIdentifier) {
-            if ($dbalSchemaTable->hasPrimaryKey() === false) {
-                // no primary key
-            } elseif (in_array($columnIdentifier, $dbalSchemaTable->getPrimaryKeyColumns())) {
-                $primaryKeyWhere[] = $this->makeArrayMappingToProperty($columnIdentifier, $columnIdentifier);
-            }
-        }
-
-        $methods['__set'] = $this->describeMethod(false, ["property" => 'string', "value" => 'string'], [
-            'if (count($this->table->update([$property => $this->__get($property)], $this->primaryKey())) > 0) {',
-            '$this->values[$property] = $value;',
-            '}'
-        ]);
-        $methods['__get'] = $this->describeMethod(false, ["property" => 'string'], [
-            'return $this->values[$property];'
-        ]);
-        $methods['primaryKey'] = $this->describeMethod(false, [], [
-            'return [' . join(', ', $primaryKeyWhere) . '];'
-        ]);
-
-
-        $methods['delete'] = $this->describeMethod(false, [], [
-            'return $this->table->delete($this->primaryKey());'
-        ]);
 
         foreach ($dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
-            $words = explode('_', $foreignKeyIdentifier);
-            $camelCased = array_map('ucfirst', $words);
-            $foreignKeyMethodIdentifier = join('', $camelCased);
-
-            $fkLocalColumns = $foreignKey->getLocalColumns();
-            $where = array_combine($foreignKey->getForeignColumns(), $fkLocalColumns);
-
-            $whereParameters = [];
-            foreach ($where as $referencedColumnName => $parameterIdentifier) {
-                $whereParameters[] = $this->makeArrayMappingToProperty($referencedColumnName, $parameterIdentifier);
-            }
-
-            $query = ['return $this->table->selectFrom("' . $foreignKey->getForeignTableName() . '", [\'' . join('\', \'', $foreignKey->getForeignColumns()) . '\'], [', join(',' . PHP_EOL, $whereParameters), ']);'];
-            
-            $methods["fetchBy" . $foreignKeyMethodIdentifier] = $this->describeMethod(false, [], $query);
+            $methods["fetchBy" . join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = $this->describeFetchByFKMethod($foreignKey);
         }
         
         return [
-            'identifier' => $this->namespace . $tableIdentifier,
+            'identifier' => $this->namespace . $dbalSchemaTable->getName(),
             'interfaces' => ['\\ActiveRecord\\WritableRecord'],
-            'properties' => $properties,
+            'properties' => [
+                'table' => ['\ActiveRecord\Table', ['static' => false, 'value' => null]],
+                'values' => ['array', ['static' => false, 'value' => null]]
+            ],
             'methods' => $methods
         ];
     }
