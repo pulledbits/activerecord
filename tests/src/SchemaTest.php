7961a353bfefaefac28613319b6b0a2c9c6a70de
Rik Meijer <rikmeijer@gmail.com>
2017-01-11 14:37:43 +0100

    more reuse of tested object property

--- tests/src/SchemaTest.php
+++ tests/src/SchemaTest.php
@@ -19,7 +19,14 @@ class SchemaTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->object = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
+        $this->object = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id AS _id, name AS _name FROM activiteit WHERE id = :param1/', [
+                [],
+                [],
+                [],
+                [],
+                []
+            ]
+        ));
     }
 
     public function testTransformTableIdentifierToRecordClassIdentifier_When_TableIdentifierSupplied_Expect_TableIdPrefixedWithTargetNamespace()
@@ -53,17 +60,7 @@ class SchemaTest extends \PHPUnit_Framework_TestCase
 
     public function testExecute_When_WhenProperQueryWithNamedParametersSupplied_Expect_PDOStatementWithFiveRecords()
     {
-        $schema = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('/SELECT id AS _id, name AS _name FROM activiteit WHERE id = :param1/', [
-                [],
-                [],
-                [],
-                [],
-                []
-            ]
-        ));
-
-        $statement = $schema->execute('SELECT id AS _id, name AS _name FROM activiteit WHERE id = :param1', [':param1' => '1']);
-
+        $statement = $this->object->execute('SELECT id AS _id, name AS _name FROM activiteit WHERE id = :param1', [':param1' => '1']);
         $this->assertCount(5, $statement->fetchAll(\PDO::FETCH_ASSOC));
     }
 }
