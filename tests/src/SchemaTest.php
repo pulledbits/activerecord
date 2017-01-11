1dfbd18dd2099958731eef85dc890fabc0dbcbcd
Rik Meijer <rikmeijer@gmail.com>
2017-01-11 14:36:18 +0100

    reduce code duplication in SchemaTest

--- tests/src/SchemaTest.php
+++ tests/src/SchemaTest.php
@@ -12,44 +12,41 @@ namespace ActiveRecord;
 class SchemaTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var Schema
+     */
+    private $object;
+
+    protected function setUp()
+    {
+        $this->object = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
+    }
+
     public function testTransformTableIdentifierToRecordClassIdentifier_When_TableIdentifierSupplied_Expect_TableIdPrefixedWithTargetNamespace()
     {
-
-        $schema = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
-
-        $this->assertEquals('\Test\Record\activiteit', $schema->transformTableIdentifierToRecordClassIdentifier('activiteit'));
+        $this->assertEquals('\Test\Record\activiteit', $this->object->transformTableIdentifierToRecordClassIdentifier('activiteit'));
     }
 
     public function testPrepareParameters_When_ColumnIdentifiersSupplied_Expect_ColumnIdPrefixedWithUnderscore()
     {
-
-        $schema = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
-
         $namedParameter = ':' . sha1('where_id');
-
-        $this->assertEquals([["id" => "id = " . $namedParameter], [$namedParameter => '1']], $schema->prepareParameters('where', ['id' => '1']));
+        $this->assertEquals([["id" => "id = " . $namedParameter], [$namedParameter => '1']], $this->object->prepareParameters('where', ['id' => '1']));
     }
 
     public function testMakeWhereCondition_When_NoColumnIdentifiersAndValuesSupplied_Expect_NoProperSQLWhereConditionAndNamedParameters()
     {
-
-        $schema = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
-
         $namedParameters = [];
-        $this->assertEquals("", $schema->makeWhereCondition([], $namedParameters));
+        $this->assertEquals("", $this->object->makeWhereCondition([], $namedParameters));
         $this->assertCount(0, $namedParameters);
     }
 
     public function testMakeWhereCondition_When_ColumnIdentifiersAndValuesSupplied_Expect_ProperSQLWhereConditionAndNamedParameters()
     {
-
-        $schema = new Schema('\Test\Record', \ActiveRecord\Test\createMockPDO('', []));
-
         $namedParameterId = ':' . sha1('where_id');
         $namedParameterName = ':' . sha1('where_name');
 
         $namedParameters = [];
-        $this->assertEquals(" WHERE id = $namedParameterId AND name = $namedParameterName", $schema->makeWhereCondition(['id' => '1', 'name' => 'MYName'], $namedParameters));
+        $this->assertEquals(" WHERE id = $namedParameterId AND name = $namedParameterName", $this->object->makeWhereCondition(['id' => '1', 'name' => 'MYName'], $namedParameters));
         $this->assertEquals('1', $namedParameters[$namedParameterId]);
         $this->assertEquals('MYName', $namedParameters[$namedParameterName]);
     }
