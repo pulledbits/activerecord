235b982613948c0d01a7410fe8687045ec6a526d
Rik Meijer <rikmeijer@gmail.com>
2017-02-21 15:34:55 +0100

    enhance documentation

--- src/Record.php
+++ src/Record.php
@@ -9,12 +9,29 @@ namespace ActiveRecord;
 
 interface Record
 {
+    /**
+     * @param string $referenceIdentifier
+     * @param string $referencedEntityTypeIdentifier
+     * @param array $conditions
+     * @return void
+     */
     public function references(string $referenceIdentifier, string $referencedEntityTypeIdentifier, array $conditions);
 
+    /**
+     * @param array $values
+     * @return void
+     */
     public function contains(array $values);
 
+    /**
+     * @param array $attributeIdentifiers
+     * @return void
+     */
     public function requires(array $attributeIdentifiers);
 
+    /**
+     * @return bool
+     */
     public function missesRequiredValues() : bool;
 
     /**
@@ -22,8 +39,18 @@ interface Record
      */
     public function __get($property);
 
+    /**
+     * @param string $entityTypeIdentifier
+     * @param array $conditions
+     * @return Record[]
+     */
     public function read(string $entityTypeIdentifier, array $conditions): array;
 
+    /**
+     * @param string $entityTypeIdentifier
+     * @param array $conditions
+     * @return Record
+     */
     public function readFirst(string $entityTypeIdentifier, array $conditions): \ActiveRecord\Record;
 
     /**
@@ -33,10 +60,19 @@ interface Record
     public function __set($property, $value);
 
     /**
+     * @return int
      */
     public function delete() : int;
 
+    /**
+     * @return int
+     */
     public function create() : int;
 
+    /**
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
     public function __call(string $method, array $arguments);
 }
