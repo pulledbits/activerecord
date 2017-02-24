ea9454ccd103c92af10e6a6865880252aeab99c3
Rik Meijer <rikmeijer@gmail.com>
2017-02-24 19:47:15 +0100

    lose obsolete variables

--- src/SQL/Source/Table.php
+++ src/SQL/Source/Table.php
@@ -8,29 +8,29 @@ final class Table
      * @return array
      */
     public function describe(\Doctrine\DBAL\Schema\Table $dbalSchemaTable) : array {
-        $primaryKeyColumns = [];
+        $description = [
+            'identifier' => [],
+            'requiredAttributeIdentifiers' => [],
+            'references' => []
+        ];
+
         if ($dbalSchemaTable->hasPrimaryKey()) {
-            $primaryKeyColumns = $dbalSchemaTable->getPrimaryKeyColumns();
+            $description['identifier'] = $dbalSchemaTable->getPrimaryKeyColumns();
         }
 
-        $requiredAttributeIdentifiers = [];
         foreach ($dbalSchemaTable->getColumns() as $columnIdentifier => $column) {
             if ($column->getAutoincrement()) {
                 continue;
             } elseif ($column->getNotnull()) {
-                $requiredAttributeIdentifiers[] = $columnIdentifier;
+                $description['requiredAttributeIdentifiers'][] = $columnIdentifier;
             }
         }
-        $references = [];
+
         foreach ($dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
-            $references[join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = $this->makeReference($foreignKey->getForeignTableName(), array_combine($foreignKey->getForeignColumns(), $foreignKey->getLocalColumns()));
+            $description['references'][join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = $this->makeReference($foreignKey->getForeignTableName(), array_combine($foreignKey->getForeignColumns(), $foreignKey->getLocalColumns()));
         }
 
-        return [
-            'identifier' => $primaryKeyColumns,
-            'requiredAttributeIdentifiers' => $requiredAttributeIdentifiers,
-            'references' => $references
-        ];
+        return $description;
     }
 
     public function makeReference(string $entityTypeIdentifier, array $conditions) : array
