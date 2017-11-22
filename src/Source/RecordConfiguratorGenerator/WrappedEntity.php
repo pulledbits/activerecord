89bd62345c48515665aad3d10268f01cf36f3f56
Rik Meijer <rikmeijer@gmail.com>
2017-11-22 11:45:24 +0100

    pass $recordFactory to wrapped entity configurator

--- src/Source/RecordConfiguratorGenerator/WrappedEntity.php
+++ src/Source/RecordConfiguratorGenerator/WrappedEntity.php
@@ -16,6 +16,6 @@ final class WrappedEntity implements RecordConfiguratorGenerator
 
     public function generateConfigurator(StreamInterface $stream) : void
     {
-        $stream->write(PHP_EOL . 'return $this->generate("' . $this->entityTypeIdentifier . '");');
+        $stream->write(PHP_EOL . 'return $this->generate($recordFactory, "' . $this->entityTypeIdentifier . '");');
     }
 }
