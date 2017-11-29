9dc790971dd52b587bb4f7af351b5af10be7e778
Rik Meijer <rikmeijer@gmail.com>
2017-11-29 11:29:36 +0100

    move code to TableDescription

--- src/SQL/Meta/Table.php
+++ src/SQL/Meta/Table.php
@@ -4,32 +4,11 @@ namespace pulledbits\ActiveRecord\SQL\Meta;
 final class Table implements \pulledbits\ActiveRecord\Source\Table
 {
     public function describe(\Doctrine\DBAL\Schema\Table $dbalSchemaTable) : TableDescription {
-        $description = new TableDescription([], [], []);
-
-        if ($dbalSchemaTable->hasPrimaryKey()) {
-            $description->identifier = $dbalSchemaTable->getPrimaryKeyColumns();
-        }
-
-        foreach ($dbalSchemaTable->getColumns() as $columnIdentifier => $column) {
-            if ($column->getAutoincrement()) {
-                continue;
-            } elseif ($column->getNotnull()) {
-                $description->requiredAttributeIdentifiers[] = $columnIdentifier;
-            }
-        }
-
-        foreach ($dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
-            $description->references[join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = $this->makeReference($foreignKey->getForeignTableName(), array_combine($foreignKey->getForeignColumns(), $foreignKey->getLocalColumns()));
-        }
-
-        return $description;
+        return TableDescription::makeFromDBALTable($dbalSchemaTable);
     }
 
-    public function makeReference(string $entityTypeIdentifier, array $conditions) : array
+    public function makeReference(string $entityTypeIdentifier, array $conditions): array
     {
-        return [
-            'table' => $entityTypeIdentifier,
-            'where' => $conditions
-        ];
+        return TableDescription::makeReference($entityTypeIdentifier, $conditions);
     }
 }
--- src/SQL/Meta/TableDescription.php
+++ src/SQL/Meta/TableDescription.php
@@ -9,4 +9,35 @@ class TableDescription extends Struct
     public $identifier = [];
     public $requiredAttributeIdentifiers = [];
     public $references = [];
+
+    static function makeFromDBALTable(\Doctrine\DBAL\Schema\Table $dbalSchemaTable) : self
+    {
+        $description = new self([], [], []);
+
+        if ($dbalSchemaTable->hasPrimaryKey()) {
+            $description->identifier = $dbalSchemaTable->getPrimaryKeyColumns();
+        }
+
+        foreach ($dbalSchemaTable->getColumns() as $columnIdentifier => $column) {
+            if ($column->getAutoincrement()) {
+                continue;
+            } elseif ($column->getNotnull()) {
+                $description->requiredAttributeIdentifiers[] = $columnIdentifier;
+            }
+        }
+
+        foreach ($dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
+            $description->references[join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = self::makeReference($foreignKey->getForeignTableName(), array_combine($foreignKey->getForeignColumns(), $foreignKey->getLocalColumns()));
+        }
+
+        return $description;
+    }
+
+    static function makeReference(string $entityTypeIdentifier, array $conditions) : array
+    {
+        return [
+            'table' => $entityTypeIdentifier,
+            'where' => $conditions
+        ];
+    }
 }
