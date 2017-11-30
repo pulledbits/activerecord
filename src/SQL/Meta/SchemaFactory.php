b0723933d037f3799c6eb44311b3c4b9d903cb72
Rik Meijer <rikmeijer@gmail.com>
2017-11-30 11:07:53 +0100

    move dbal conversion code to schemafactory so I can remove DBAL dependency bit-by-bit

--- src/SQL/Meta/SchemaFactory.php
+++ src/SQL/Meta/SchemaFactory.php
@@ -2,6 +2,7 @@
 namespace pulledbits\ActiveRecord\SQL\Meta;
 
 use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Doctrine\DBAL\Schema\Table;
 use pulledbits\ActiveRecord\SQL\Connection;
 
 class SchemaFactory
@@ -19,7 +20,25 @@ class SchemaFactory
         $tables = [];
         $fullTables = $connection->execute('SHOW FULL TABLES WHERE Table_type = \'BASE TABLE\'', []);
         foreach ($fullTables->fetchAll() as $baseTable) {
-            $tables[$baseTable[0]] = TableDescription::makeFromDBALTable($schemaManager->listTableDetails($baseTable[0]));
+            $dbalSchemaTable = $schemaManager->listTableDetails($baseTable[0]);
+
+            $tables[$baseTable[0]] = new TableDescription([], [], []);
+
+            if ($dbalSchemaTable->hasPrimaryKey()) {
+                $tables[$baseTable[0]]->identifier = $dbalSchemaTable->getPrimaryKeyColumns();
+            }
+
+            foreach ($dbalSchemaTable->getColumns() as $columnIdentifier => $column) {
+                if ($column->getAutoincrement()) {
+                    continue;
+                } elseif ($column->getNotnull()) {
+                    $tables[$baseTable[0]]->requiredAttributeIdentifiers[] = $columnIdentifier;
+                }
+            }
+
+            foreach ($dbalSchemaTable->getForeignKeys() as $foreignKeyIdentifier => $foreignKey) {
+                $tables[$baseTable[0]]->references[join('', array_map('ucfirst', explode('_', $foreignKeyIdentifier)))] = TableDescription::makeReference($foreignKey->getForeignTableName(), array_combine($foreignKey->getForeignColumns(), $foreignKey->getLocalColumns()));
+            }
         }
         $prototypeTables = $tables;
         foreach ($tables as $tableName => $recordClassDescription) {
